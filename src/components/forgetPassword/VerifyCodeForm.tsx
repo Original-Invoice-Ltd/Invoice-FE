6d3945d259e1e91fdf00b377fac131321eda0a4f
aondofe014704 <songujack@gmail.com>
2025-11-16 04:38:31 +0100

    correct ref assignment type in VerifyCodeForm to resolve Next.js TypeScript build error

--- src/components/forgetPassword/VerifyCodeForm.tsx
+++ src/components/forgetPassword/VerifyCodeForm.tsx
@@ -90,7 +90,7 @@ export default function VerifyCodeForm({ email, onCodeComplete, onResendCode, on
         {code.map((digit, index) => (
           <input
             key={index}
-            ref={(el) => (inputRefs.current[index] = el)}
+            ref={(el) => { inputRefs.current[index] = el; }}
             type="text"
             inputMode="numeric"
             maxLength={1}
@@ -138,4 +138,3 @@ export default function VerifyCodeForm({ email, onCodeComplete, onResendCode, on
     </form>
   );
 }
-
