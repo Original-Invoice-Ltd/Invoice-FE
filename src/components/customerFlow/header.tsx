7384acb0858958d3f5917f664fbf72688cab962e
aondofe014704 <songujack@gmail.com>
2026-01-02 17:42:10 +0100

    Laid Foundation for customer Flow

---
+++ src/components/customerFlow/header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return( 
+    <div className="flex justify-between items-center h-[64px] ml-[268px] px-8 bg-[#EFF8FF] border-b border-[#E4E7EC]">
+  <h1>James Brown</h1>
+</div>
+
+    );
+}
+
+export default Header;
